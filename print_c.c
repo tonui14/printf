1e9a3e6e4b87749d54f38fe49f804aa23b56a310
Daisy Tonui <tonuidaisy@gmail.com>
2023-10-17 03:00:47 -0700

    print_c.c

--- print_c.c
+++ print_c.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
+/**
+ * print_c - is a function that prints a character
+ * It also increments value pointed to by 'trace'
+ *
+ * @c: character to be printed
+ * @trace: a pointer to the integer for tracking
+ **/
+
 void print_c(char c, int *trace)
 {
-    _putchar(c);
-    (*trace)++;
+	_putchar(c);
+	(*trace)++;
 }
